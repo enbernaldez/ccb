7cbe1f19b05a46cdba13813cb49e4700a58ea6e9
enbernaldez <127641666+enbernaldez@users.noreply.github.com>
2023-05-06 14:09:02 +0800

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -1,19 +1,31 @@
 <!--Navigation Bar-->
     <nav class="navbar">
         <div class="container">
-            <a class="active" href="landing_page.php">
-                <img src="pics/CCB Logo.png" width="50px" height="50px"/>
+            <a class="active" 
+               href="landing_page.php">
+                <img src="pics/CCB Logo.png" 
+                     width="50px" 
+                     height="50px"/>
                 Chew Chew Bakeshop
             </a>
-            <li>
+            <dl style="padding-top: 12px;">
                 <a href="products.php">Products</a>
                 <?php
                 if (isset($_SESSION['user_id'])) {
-                    echo "<a class='ms-5' href='orders.php'>Orders</a>";
-                    echo "<a class='mx-5' href=''>" . $_SESSION['username'] . "</a>";
-                    echo "<a href='logout.php'>Log Out</a>";
+                ?>
+                    
+                    <a class='ms-5' 
+                       href='orders.php'>
+                        Orders
+                    </a>
+                    <a class='mx-5' 
+                       href=''>
+                        <?php echo $_SESSION['username']; ?>
+                    </a>
+                    <a href='logout.php'>Log Out</a>
+                <?php
                 }
                 ?>
-            </li>
+            </dl>
         </div>
     </nav>
