0be6b42ca3f14d89a688cd24e116ef427ecf034f
enbernaldez <127641666+enbernaldez@users.noreply.github.com>
2023-05-07 19:58:48 +0800

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -1,17 +1,20 @@
 <?php
 include_once "db_conn.php";
 
+//for confirmation
 if(isset($_POST['reference_number'])) {
     
+    date_default_timezone_set("Asia/Manila");
     $o_order_id = $_POST['order_id'];
     $o_ref_num = $_POST['reference_number'];
-    
-    if(!is_existing($conn, $o_ref_num, 'order_ref_num', 'orders')) {
-        foreach ($o_order_id as $value) {
+    //confirm order
+    if(isset($_POST['submit'])) {
+        foreach($o_order_id as $value) {
 
             $table = "orders";
             $fields = array( 'order_status' => 'P'
                            , 'order_ref_num' => $o_ref_num
+                           , 'last_update' => date("Y-m-d H:i:s")
                            );
             $filter = array( 'order_id' => $value );
 
@@ -25,6 +28,12 @@ if(isset($_POST['reference_number'])) {
             header("location: orders.php?confirm_order=failed");
             exit();
         }
+    }
+    //cancel
+    if(isset($_POST['delete'])) {
+        unset($_POST['submit']);
+        header("location: orders.php");
+        exit;
     }
 }
 
