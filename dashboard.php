04b334fc8077df0aa72e819c3a1fe4d2ab77bc83
enbernaldez <127641666+enbernaldez@users.noreply.github.com>
2023-05-10 23:15:44 +0800

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <?php include_once "db_conn.php"; ?>
+    <head>
+        <meta charset="UTF-8">
+        <title>CCB Overview</title>
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="bootstrap-icons-1.9.1/bootstrap-icons.css">
+        <link rel="stylesheet" href="ccb.css">
+    </head>
+    <body>
+    <!--Navigation Bar-->
+        <?php include_once "navbar.php"; ?>
+        
+        <div class="container pt-5">
+            <div class="row pt-3">
+                
+            </div>
+        </div>
+    </body>
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+    <script type="text/javascript" src="js/jquery-3.5.1.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="ccb.js"></script>
+</html>
