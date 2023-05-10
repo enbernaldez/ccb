f5d957d1d36e298c7d690896391f6fb019022d76
enbernaldez <127641666+enbernaldez@users.noreply.github.com>
2023-05-11 00:06:17 +0800

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -1,8 +1,29 @@
 <?php
 if(isset($_SESSION['user_id'])) {
-    $home = "home_page.php";
+    $type = $_SESSION['user_type'];
+    
+    switch ($type) {
+        case "A":
+            $home = "overview.php";
+            $products = "product_index.php";
+            $orders = "order_index.php?pending";
+            break;
+        case "D":
+            $home = "dashboard.php";
+            $orders = "delivery.php?deliver";
+            break;
+        case "C":
+            $home = "home_page.php";
+            $products = "products.php";
+            $orders = "orders.php";
+            break;
+        default:
+            $home = "landing_page.php";
+            $products = "products.php";
+    }
 } else {
     $home = "landing_page.php";
+    $products = "products.php";
 }
 ?>
 <!--Navigation Bar-->
@@ -16,21 +37,39 @@ if(isset($_SESSION['user_id'])) {
                 Chew Chew Bakeshop
             </a>
             <dl style="padding-top: 12px;">
-                <a href="products.php">Products</a>
                 <?php
-                if (isset($_SESSION['user_id'])) {
-                ?>
-                    
-                    <a class='ms-5' 
-                       href='orders.php'>
-                        Orders
-                    </a>
-                    <a class='mx-5' 
-                       href=''>
-                        <?php echo $_SESSION['username']; ?>
-                    </a>
-                    <a href='logout.php'>Log Out</a>
-                <?php
+                if($_SESSION['user_type'] != 'D') {
+                    echo "
+                    <a href=" . $products . ">Products</a>";
+                }
+                if(isset($_SESSION['user_id'])) {
+                    echo "
+                        <a class='ms-5' 
+                           href='" . $orders . "'>
+                            Orders
+                        </a>";
+                    if($_SESSION['user_type'] == 'A') {
+                        echo "
+                            <a class='ms-5' 
+                               href='user_index.php?customer'>
+                                Users
+                            </a>
+                            <a class='ms-5' 
+                               href='reports.php?today'>
+                                Reports
+                            </a>";
+                    }
+                    echo "
+                        <a class='mx-5' 
+                           href=''>
+                            " . $_SESSION['username'] . "
+                        </a>
+                        <a href='logout.php'>Log Out</a>";
+                }
+                if($_SESSION['user_type'] != 'A') {
+                    echo "
+                    <a class='ms-5'
+                       href=about_us.php>About Us</a>";
                 }
                 ?>
             </dl>
